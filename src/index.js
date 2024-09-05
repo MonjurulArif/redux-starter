5aba985f29824f31a8190b445ad13c1f6c37b4a5
Monjurul Opu <monjurulopu@gmail.com>
2024-09-05 18:05:33 +0600

    Function as First Class Citizen

--- src/index.js
+++ src/index.js
@@ -1,5 +1,22 @@
- console.log("Hello!!!");
+function sayHello() {
+  return "Hello World";
+}
+
+let fn = sayHello;
+fn();
+sayHello();
+
+function greet(fnMessage) {
+  console.log("From greet: ", fnMessage());
+}
+
+greet(sayHello); 
 
 
+function sayHello() {
+  return function () {
+    return "Hello World";
+  }
+}
 
  
