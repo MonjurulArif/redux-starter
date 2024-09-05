bb9847b06730792133411ef113c791197453b3e9
Monjurul Arif <monjurulopu@gmail.com>
2024-09-05 18:23:12 +0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,22 +1,2 @@
-function sayHello() {
-  return "Hello World";
-}
-
-let fn = sayHello;
-fn();
-sayHello();
-
-function greet(fnMessage) {
-  console.log("From greet: ", fnMessage());
-}
-
-greet(sayHello); 
-
-
-function sayHello() {
-  return function () {
-    return "Hello World";
-  }
-}
-
+console.log("Hello JavaScritp");
  
