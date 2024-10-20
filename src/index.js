52a10d37afcb42b5547701478c9bd4b330a9baf9
Monjurul Opu <monjurulopu@gmail.com>
2024-10-20 16:54:59 +0600

    Dispatching Actions

--- src/index.js
+++ src/index.js
@@ -1,2 +1,19 @@
+import store from "./store";
 
-console.log("JavaScript"); 
+console.log('store:', store);
+
+store.dispatch({
+  type: "bugAdded",
+  payload: {
+    description: "Bug1"
+  }
+});
+
+store.dispatch({
+  type: "bugRemoved",
+  payload: {
+    id: 1
+  }
+});
+
+console.log('store.getState():', store.getState());
