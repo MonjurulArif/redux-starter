343a54c1a130652e524daf92dc61b1b36acc1687
Monjurul Opu <monjurulopu@gmail.com>
2024-10-23 14:46:41 +0600

    Action Creators 2

--- src/actions.js
+++ src/actions.js
@@ -4,7 +4,16 @@ import * as actions from "./actionTypes";
 export const bugAdded = description =>  ({
   type: actions.BUG_ADDED,
     payload: {
-      description: "Bug1"
+      // description: description
+      description    // TODO:  if the property and value are same write shorthand syntax
+    }
+})
+
+export const bugResolved = id => ({
+  type: actions.BUG_RESOLVED,
+    payload: {
+      // id: id
+      id     // TODO:  if the property and value are same write shorthand syntax
     }
 })
 
